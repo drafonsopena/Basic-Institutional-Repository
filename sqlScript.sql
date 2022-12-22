3fb3548cfc138c2cb4b34dc0ba09ed7c4ceb9ca1
Dr. Afonso Pena <afonsoispena@outlook.com>
2022-12-22 14:54:09 +0100

    Update sqlScript.sql

--- sqlScript.sql
+++ sqlScript.sql
@@ -6,7 +6,7 @@ user_email VARCHAR(250) NOT NULL,
 user_pswd VARCHAR (20) NOT NULL,
 user_id INT (20) NOT NULL ,PRIMARY KEY(user_id);
 
--- CREATE and DROP TABLE
+/*-- CREATE and DROP TABLE --*/
 DROP TABLE `project_article`;
 CREATE TABLE IF NOT EXISTS `project_article` (
 	`proj_id` INT (50) NOT NULL PRIMARY KEY AUTO_INCREMENT,
@@ -16,18 +16,3 @@ CREATE TABLE IF NOT EXISTS `project_article` (
 	`proj_title` VARCHAR (255) NOT NULL,
 	`proj_file` LONGBLOB NOT NULL, 
 	`upload_date` DATETIME NOT NULL);
-
-
-
-
-
-
-
-
-`proj_file` BLOB NOT NULL (in case we need to upload a whole file to the database)
-
-Restart Auto INCREMENT
-
-TRUNCATE TABLE table_name;
-*/
-
