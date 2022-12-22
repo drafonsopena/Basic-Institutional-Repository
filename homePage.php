79df79b0d883e670ab4368bccefe01622f687653
Dr. Afonso Pena <afonsoispena@outlook.com>
2022-12-22 14:42:39 +0100

    This is the home page.
    This is the home page. It is the main page that gives access to the navigation menu and other.

---
+++ homePage.php
@@ -0,0 +1,110 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8"/>
+    
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="icon" type="image/x-icon" href="../images/website_icon.ico">
+<link rel ="stylesheet" href = "../cssScripts/homeStyle.css"/>
+
+
+<title>HOME PAGE</title>
+</head>
+
+<body>
+
+
+<!--Start Navigation Bar--> 
+
+<section class ="navSection">
+
+	<a href ="homePage.php">Home</a>
+	
+	<a href ="reg_log/adminPage.php">Profile</a>
+	
+	<a href ="reg_log/loginPage.php">Login</a>
+		
+	<a href ="reg_log/registerPage.php">Register</a>
+
+</section> 
+<!--End Navigation Bar--> 
+
+
+<!--Start Main Content--> 
+<section class ="backgroundImg">
+  <img  src="../images/ium-erongo.jpeg" alt ="ium-Research-Center">
+
+
+
+<div class="transBox">
+	<h2>"Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit..."</h2>
+  
+	<h2>"There is no one who loves pain itself, who seeks after it and wants to have it, simply because it is pain..."</h2>
+  
+	<h2>Wanna see more?</h2>
+	<p><button type = "submit"><a href="upload_pdf_files/explorePage.php">Search and Browse</a></button></p>
+
+	
+</div>
+</section>
+
+<div class ="center">
+<img src="../images/runIT-again.jpg" alt="Run IT Again" style="height: 25%; width: 20%">
+<h1 class="text-dark">What is an Institutional Repositiory?</h1>
+
+<p>
+Institutional Repositories  are digital collections of archives used for keeping the intellectual output of a single or multi-university community. These intellectual output include research papers,  monographs, academic articles, Electronic Theses and Dissertations (ETDs), and other digital assets published during an academic year. IRs can have many forms and serve a variety of purposes. IUM-RPER focuses on a digital library that stores the research output of all academic levels offered in the Faculty of Information and Technology like Bachelors, Honor Degree, Masters and Doctorate. 
+</p>
+<p>
+This study was intended for the development of an Institutional Repository that can collect, manage, store, preserve and provide open access to research outputs from the IUM community.
+IRs are digital collections of archives used for capturing and keeping the intellectual output of a single or multi-university community. These research outputs include research papers, monographs, academic articles, ETDs and other digital assets published during an academic year (LISBDNETWORK, 2018).
+</p>
+
+</div>
+
+<div class ="center">
+<img src="../images/ium-logo.jpg" class="img-fluid rounded-1" alt="IUM LOGO" style="height: 25%; width: 20%">
+
+
+
+<h1>Proven &amp; Reputable Track Record</h1>
+
+<p>
+ Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+</p>
+
+</div>
+<!--End Main Content--> 
+
+
+<!--Start License Section-->
+<section >
+<div class="licenseSection">
+
+
+  <h1>SOFTWARE LICENCE</h1>
+  <br/>
+  
+  <p>MIT License</p>
+
+
+<p>Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions: The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.</p>
+
+<p>THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.</p>
+</div>
+</section>
+<!--End License Section-->
+
+
+<!--Start Footer-->
+<div class ="footer">
+<footer>
+	<p>Copyright &copy; 2022 <a href ="https://github.com/drafonsopena?tab=repositories" target ="blank">Afonso "AvvA4LLLL" Pena</a></p>
+</footer>
+</div>
+<!--End Footer-->
+
+
+</body>
+</html>
